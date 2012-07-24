6b7ec1c4e246ca96e57053ed503514a79d35df5e
Manindra Moharana <mkmoharana29@gmail.com>
2012-07-24 15:25:54 +0530

    Updated for revision 9871. processing.opengl.* is now a default import.

--- xqmodeData/XQMode/src/quarkninja/mode/xqmode/XQPreprocessor.java
+++ xqmodeData/XQMode/src/quarkninja/mode/xqmode/XQPreprocessor.java
@@ -9,13 +9,10 @@ import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.NumberLiteral;
-import org.eclipse.jdt.core.dom.PrimitiveType;
 import org.eclipse.jdt.core.dom.SimpleType;
-import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.Document;
@@ -53,7 +50,7 @@ public class XQPreprocessor {
 		cu.recordModifications();
 		rewrite = ASTRewrite.create(cu.getAST());
 		cu.accept(new XQASTVisitor());
-		
+
 		TextEdit edits = cu.rewrite(doc, null);
 		try {
 			edits.apply(doc);
@@ -62,9 +59,9 @@ public class XQPreprocessor {
 		} catch (BadLocationException e) {
 			e.printStackTrace();
 		}
-//		 System.out.println("------------XQPreProc-----------------");
-//		 System.out.println(doc.get());
-//		 System.out.println("------------XQPreProc End-----------------");
+		// System.out.println("------------XQPreProc-----------------");
+		// System.out.println(doc.get());
+		// System.out.println("------------XQPreProc End-----------------");
 
 		// Calculate main class offset
 		int position = doc.get().indexOf("{") + 1;
@@ -101,7 +98,8 @@ public class XQPreprocessor {
 	}
 
 	public String[] getCoreImports() {
-		return new String[] { "processing.core.*", "processing.data.*" };
+		return new String[] { "processing.core.*", "processing.data.*",
+				"processing.opengl.*" };
 	}
 
 	public String[] getDefaultImports() {
@@ -111,13 +109,13 @@ public class XQPreprocessor {
 	}
 
 	/**
-	 * <LI>Any function not specified as being protected
-	 * or private will be made 'public'. This means that <TT>void setup()</TT>
-	 * becomes <TT>public void setup()</TT>.
+	 * <LI>Any function not specified as being protected or private will be made
+	 * 'public'. This means that <TT>void setup()</TT> becomes
+	 * <TT>public void setup()</TT>.
 	 * 
-	 * <LI>Converts doubles into floats, i.e. 12.3 becomes 12.3f so that people don't have to
-	 * add f after their numbers all the time since it's confusing for
-	 * beginners. Also, most functions of p5 core deal with floats only.
+	 * <LI>Converts doubles into floats, i.e. 12.3 becomes 12.3f so that people
+	 * don't have to add f after their numbers all the time since it's confusing
+	 * for beginners. Also, most functions of p5 core deal with floats only.
 	 * 
 	 * @author Manindra Moharana
 	 * 
@@ -127,11 +125,11 @@ public class XQPreprocessor {
 		public boolean visit(MethodDeclaration node) {
 			if (node.getReturnType2() != null) {
 				// if return type is color, make it int
-//				if (node.getReturnType2().toString().equals("color")) {
-//					System.err.println("color type detected!");
-//					node.setReturnType2(rewrite.getAST().newPrimitiveType(
-//							PrimitiveType.INT));
-//				}
+				// if (node.getReturnType2().toString().equals("color")) {
+				// System.err.println("color type detected!");
+				// node.setReturnType2(rewrite.getAST().newPrimitiveType(
+				// PrimitiveType.INT));
+				// }
 
 				// The return type is not void, no need to make it public
 				// if (!node.getReturnType2().toString().equals("void"))
@@ -167,30 +165,31 @@ public class XQPreprocessor {
 			return true;
 		}
 
-//		public boolean visit(FieldDeclaration node) {
-//			if (node.getType().toString().equals("color")){
-//				System.err.println("color type detected!");
-//				node.setType(rewrite.getAST().newPrimitiveType(
-//						PrimitiveType.INT));
-//			}
-//			return true;
-//		}
-//
-//		public boolean visit(VariableDeclarationStatement node) {
-//			if (node.getType().toString().equals("color")){
-//				System.err.println("color type detected!");
-//				node.setType(rewrite.getAST().newPrimitiveType(
-//						PrimitiveType.INT));
-//			}
-//			return true;
-//		}
-		
-		public boolean visit(SimpleType node){
-			if (node.toString().equals("color")){
-				System.err.println("color type detected! :O \nThis shouldn't be happening!");
+		// public boolean visit(FieldDeclaration node) {
+		// if (node.getType().toString().equals("color")){
+		// System.err.println("color type detected!");
+		// node.setType(rewrite.getAST().newPrimitiveType(
+		// PrimitiveType.INT));
+		// }
+		// return true;
+		// }
+		//
+		// public boolean visit(VariableDeclarationStatement node) {
+		// if (node.getType().toString().equals("color")){
+		// System.err.println("color type detected!");
+		// node.setType(rewrite.getAST().newPrimitiveType(
+		// PrimitiveType.INT));
+		// }
+		// return true;
+		// }
+
+		public boolean visit(SimpleType node) {
+			if (node.toString().equals("color")) {
+				System.err
+						.println("color type detected! :O \nThis shouldn't be happening!");
 			}
-			return true; 
-			
+			return true;
+
 		}
 
 	}
